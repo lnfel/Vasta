24839d52426b177a525a1980e70404bc71b4c092
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2019-10-31 12:22:37 +0800

    packages set to under construction

--- js/custom.js
+++ js/custom.js
@@ -26,6 +26,18 @@
     }
   });
 
+  $('.info-link.itenerary').bind('click', function() {
+    var $anchor = $(this);
+    var link = $($anchor.attr('href'));
+    if (link.length) {
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+
+      event.preventDefault();
+    }
+  });
+
 
   //jQuery to collapse the navbar on scroll
   $(window).scroll(function() {
